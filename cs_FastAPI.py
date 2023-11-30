a36de4b56cfa36562fdae1e781937c58d42ebaa0
unknown <Gwenael Chazal@PC-TR-IDF032.tidf.lan.trimane.net>
2023-11-30 09:22:25 +0100

    Added docstrings to the API's functions

--- cs_FastAPI.py
+++ cs_FastAPI.py
@@ -3,14 +3,32 @@ from operator import getitem
 import fastapi
 import cs_APIFunctions as p7APIF
 import cs_Settings as p7dS
+import time
 
-app = fastapi.FastAPI()
+description = """
+## items
+
+You can **get** credit scores and probability of 
+default for a selection of customers (**items**).
+
+## itemlist
+
+You can **get** the list of available customers (**items**).
+"""
+summary = "Credit score and probability of default API."
+
+app = fastapi.FastAPI(title="Credit Score API",
+                      description=description,
+                      summary=summary)
+
 
 model = p7APIF.finalModel()
 apiDf = p7APIF.apiDfFunc()
 
+
 def allItems():
     return sorted(apiDf[p7dS.H_SHARED_UID].tolist())
+
 
 def scoreDict(id_, amount=None):
     try:
@@ -48,17 +66,31 @@ def scoreDict(id_, amount=None):
 
 
 @app.get("/")
-def check_connection():
+def connection_check():
+    """
+    Connection check
+    """
     return {"connection": "OK"}
 
 
 @app.get("/items/{item_id}")
 def score(item_id: int, amount: Union[str, None] = None):
+    """
+    Returns the estimated probability of default for customer 
+    **item_id** and therefore whether the credit should be 
+    granted or not.
+
+    If an **amount** for the credit is provided, 
+    it will be used for the estimation. 
+    """
     if amount is None:
         return scoreDict(id_=item_id)
     return scoreDict(id_=item_id, amount=float(amount))
 
+
 @app.get("/itemlist/")
 def all_items():
-    return {"items":allItems()}
-
+    """
+    Returns the whole list of available customers
+    """
+    return {"items": allItems()}
